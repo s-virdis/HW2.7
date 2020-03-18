f6248e8273444e56440b6cf87f16bec2ca847f63
sa <sa@wrf.su>
2020-03-18 11:01:23 +0300

    Initial Commit

---
+++ HW2.7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW2.7
+//
+//  Created by lastbyte on 18.03.2020.
+//  Copyright © 2020 lastbyte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
